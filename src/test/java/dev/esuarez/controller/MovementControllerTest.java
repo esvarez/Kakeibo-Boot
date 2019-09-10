d9dc704a801465126374127205b5b0e302f097bb
Suarez <esuarez@esuarez-mac.luxoft.com>
2019-09-10 17:28:22 -0500

    Test in work

---
+++ src/test/java/dev/esuarez/controller/MovementControllerTest.java
@@ -0,0 +1,31 @@
+package dev.esuarez.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class MovementControllerTest {
+
+    @Test
+    public void find_allMovements_OK() throws Exception {
+
+    }
+
+
+}
