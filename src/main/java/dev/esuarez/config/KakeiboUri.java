0bf248489f2f8661b0dd1eed4ba77f0a7505ce36
svarez <ericksuarezdev@gmail.com>
2019-09-30 23:31:11 -0500

    Urls api

--- src/main/java/dev/esuarez/config/KakeiboUri.java
+++ src/main/java/dev/esuarez/config/KakeiboUri.java
@@ -8,11 +8,15 @@ public class KakeiboUri {
 
     public static final String USERS_API = "/users";
 
+    public static final String BUDGET_API = "/budgets";
+
     public static final String ACCOUNT_API = "/accounts";
 
     public static final String MOVEMENT_API = "/movements";
 
     public static final String ACCOUNT_TYPE_API = "/account-types";
+
+    public static final String CONFIGURATION_API = "/configurations";
 
     public static final String MOVEMENT_CATEGORIES_API = "/movement-categories";
 
