416e86a8762198721789cad7d9d4ba7f31d613bc
Erick Suarez <ericksuarezdev@gmail.com>
2021-02-03 20:37:10 -0600

    Init commit

---
+++ src/main/java/dev/ericksuarez/paperpostingservice/PaperPostingServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.paperpostingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperPostingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperPostingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ericksuarez/paperpostingservice/PaperPostingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.paperpostingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperPostingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
