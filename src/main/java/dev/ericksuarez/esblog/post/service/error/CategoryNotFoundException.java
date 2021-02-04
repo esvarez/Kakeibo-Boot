4ba42c2d19f0a9e43e13969f49b7419e9a4177b9
Erick Suarez <ericksuarezdev@gmail.com>
2021-02-03 20:33:27 -0600

    Clean repo

--- src/main/java/dev/ericksuarez/esblog/post/service/error/CategoryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package dev.ericksuarez.esblog.post.service.error;
-
-public class CategoryNotFoundException extends NotFoundException {
-
-    public CategoryNotFoundException(Long id) {
-        super("Category id " + id );
-    }
-}
--- src/main/java/dev/ericksuarez/esblog/post/service/error/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package dev.ericksuarez.esblog.post.service.error;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String entity) {
-        super(entity + " not found.");
-    }
-}
