2b457a1428a9dfecb56324b43b37e40d6815a71f
Erick Suarez <ericksuarezdev@gmail.com>
2023-06-12 18:22:54 -0600

    Clean code

--- src/main/java/dev/ericksuarez/paper/posting/service/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.ericksuarez.paper.posting.service.repository;
-
-import dev.ericksuarez.paper.posting.service.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
